a839d407ba0c6a5b4cd6784923f5cb587bd16490
Panda <69938676+DemonPandaz2763@users.noreply.github.com>
2024-12-02 20:05:41 -0800

    Add files via upload

---
+++ persistence.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+
+# script written according to the kali.org instructions on adding persistence to a live usb
+
+bar='======================================================================================================'
+mnt=/mnt/my_usb
+
+# confirm logic. defaults to y
+confirm() {
+  if [[ -z "$confirm" ]]; then
+    confirm="y"
+  fi
+
+  if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
+  echo "Exiting..."
+  exit 0
+  fi
+}
+
+# check if user is running as root
+if [ "$(id -u)" -ne 0 ]; then
+  echo 'This script must be run as root. Run again with "sudo ./persistence.sh"'
+  exit 1
+fi
+
+# usb=/dev/sdX (replace "sdX" with user input)
+fdisk -l | grep -E 'Disk /dev/|Disk model'
+
+echo "$bar"
+echo -n '[?] Enter desired drive (eg, /dev/sdX): '
+read usb
+
+echo "$bar"
+echo "This script is designed to be run as the first step in setting up a live kali usb with persistence."
+echo "Run at your own risk. This will make a partition on $usb."
+echo ""
+echo -n "[?] Do you want to continue [Y/n]: "
+read -r confirm
+
+confirm
+
+# sudo fdisk $usb (this needs following inputs: n; p; enter; enter; enter; w)
+echo "$bar"
+echo "[+] Adding Linux partition to $usb"
+echo "$bar"
+
+echo -e "n\np\n\n\n\nw" | fdisk "$usb"
+
+echo "$bar"
+echo "[+] Finished adding Linux partition to $usb"
+echo "$bar"
+
+# sudo mkfs.ext4 -L persistence ${usb}3
+fdisk -l ${usb}
+echo ""
+echo -n "[?] Which partition is labeled 'Linux' (eg. /dev/sdb3): "
+read -r part
+
+echo ""
+echo -n "[?] '$part' was selected. Is this correct? [Y/n]: "
+read confirm
+
+confirm
+
+echo "$bar"
+echo "[+] Making ext4 persistence filesystem on $part"
+echo "$bar"
+
+mkfs.ext4 -L persistence ${part}
+
+# sudo mkdir -p /mnt/my_usb
+echo "$bar"
+echo "[+] Finished making filesystem"
+echo ""
+echo "[+] Making mount directory at $mnt"
+echo "$bar"
+
+mkdir -p ${mnt}
+
+# sudo mount ${usb}3 /mnt/my_usb
+echo "[+] Mounting point made at $mnt"
+echo ""
+echo "[+] Mounting $usb to $mnt"
+echo "$bar"
+
+mount ${part} ${mnt}
+
+# echo "/ union" | sudo tee /mnt/my_usb/persistence.conf
+echo "Mount $usb at $mnt"
+echo ""
+echo "[+] Making configuration file for persistence"
+echo "$bar"
+
+echo "/ union" | tee ${mnt}/persistence.conf
+
+# sudo umount ${usb}3
+echo "$bar"
+echo "[+] Finished making configuration file"
+echo ""
+echo "[+] Unmounting $usb from $mnt"
+echo "Reboot into persistence mode manually"
+
+umount ${part}
